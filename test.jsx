a959f8fa1d9e9e8d285defb38d2588d6fe0987d7
priyanshu sangte <priyanshu.sangte108@gmail.com>
2025-07-08 09:38:32 +0530

    first commit

---
+++ test.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const test = () => {
+  return (
+    <div>
+      https://www.youtube.com/watch?v=DkejA__UFjk&list=WL&index=6 
+https://www.youtube.com/watch?v=vg7_pJ4CMpQ&list=WL&index=5
+https://www.youtube.com/watch?v=VflaSkBuwOI&list=WL&index=4
+https://www.youtube.com/watch?v=0poeouUjyzQ&list=WL&index=3
+https://www.youtube.com/watch?v=AY6oktMypQg&list=WL&index=2
+https://www.youtube.com/watch?v=tiEazu2cLsU&list=WL&index=1
+https://www.youtube.com/watch?v=3o8cuTaw2ew&list=WL&index=15
+https://www.youtube.com/watch?v=9owWj_ph_70&list=WL&index=14
+https://www.youtube.com/watch?v=nVXMylmiU8U&list=WL&index=13
+https://www.youtube.com/watch?v=oY4ruMixXHA&list=WL&index=12
+https://www.youtube.com/watch?v=NGgF1bBt3z8&list=WL&index=11
+https://www.youtube.com/watch?v=lWRqDeS2m-g&list=WL&index=10
+
+
+PORT = 6565
+MONGO_URI ="mongodb+srv://admin:admin@cluster0.v8mcn.mongodb.net/todo-task?retryWrites=true&w=majority&appName=Cluster0"
+JWT_SECRET = abc12345
+NODE_ENV ='development'
+    </div>
+  )
+}
+
+export default test
